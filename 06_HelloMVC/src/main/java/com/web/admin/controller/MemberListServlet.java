cc9570c9f709bf3daf8230f4f7ada122f9da0a07
GDC10 <GDC10@DESKTOP-JRF4Q8D>
2023-05-31 17:55:58 +0900

    230531 06_HelloMVC
    1) manageMember.jsp에 회원리스트검색화면.txt 파일내용 추가삽입
    2) views/notice파일 생성
    2-1) 이하에 notice1,2,3생성
    //공지사항상세화면.txt, 공지사항작성화면.txt, 공지사항화면.txt 파일내용 삽입하여 추가
    (숙제 : 공지사항 기능에 등록, 수정, 삭제, 리스트출력 해보기!)
    
    Tip) 프로젝트명 바꾸면
    (Project Explorer모드)web.xml,
    (navigator모드)setting-org.eclipse.wst.common.component 속 프로젝트명 관련 값 수정해야
    관련 오류 해소될 때 있음
    
    +인텔리제이(유료 개발툴)

--- 06_HelloMVC/src/main/java/com/web/admin/controller/MemberListServlet.java
+++ 06_HelloMVC/src/main/java/com/web/admin/controller/MemberListServlet.java
@@ -30,11 +30,52 @@ public class MemberListServlet extends HttpServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
+//    protected void doGet2(HttpServletRequest request, HttpServletRequest resonse) throws ServletException, IOException{
+//    	
+//    	//페이징처리하기
+//    	int cPage;
+//    	try {
+//    		cPage=Integer.parseInt(request.getParameter("cPage"));
+//    	}catch(NumberFormatException e) {
+//    		cPage=1; // v
+//    	}
+//    	int numPerpage=10;
+//    	//1. DB에서 member테이블에 있는 데이터 가져오기
+//    	List<MemberDTO> members=new AdminService().selectMemberAll2(cPage, numPerpage);
+//    	//2. DB에서 가져온 데이터 저장
+//    	request.setAttribute("members", members);
+//    	//3. 페이지바를 구성
+//    	// 1) DB에 저장된 전체 데이터의 수를 가져오기
+//    	int totalData=new AdminService().selectMemberCount2();
+//    	// 2) 전체 페이지 수 계산하기(소수점 주의)
+//    	int totalPage=(int)Math.ceil(totalData/numPerpage);
+//    	// 3) 페이지바 시작번호 계산하기 
+//    	int pageBarSize=10;
+//    	int pageNo= ((cPage-1)/pageBarSize)*pageBarSize+1;
+//    	int pageEnd= pageNo+pageBarSize-1;
+//    	//4. 페이지바를 구성하는 html저장하기 : pageBar
+//    	String pageBar="";
+//    	if(pageNo==1) {
+//    		pageBar+="<span>[이전]</span>";
+//    	}else {
+//    		pageBar+="<a href='"+request.getRequestURI()+"?cPage="+(pageNo-1)+"'>[이전]</a>";
+//    	}
+//    	//선택할 페이지 번호 출력하기
+//    	while(!(pageNo>pageEnd)||pageNo>totalPage)){
+//    		if(pageNo==cPage) {
+//    			pageBar+="<span>"+pageNo+"</span>";
+//    		}else {
+//    			pageBar+="<a href='"+request.getRequestURI()+"?cPage="+pageNo+"'>"+pageNo+"</a>";
+//    		}
+//    	}
+//    	
+//    }
+    
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 //		System.out.println("MemberListServlet실행");
 
-		//페이징처리하기
+		//페이징처리하기 : cPage, numPerpage
 		int cPage;
 		try {
 		cPage=Integer.parseInt(request.getParameter("cPage"));// 현재(current)페이지를 저장하는 변수
@@ -46,19 +87,23 @@ public class MemberListServlet extends HttpServlet {
 		//DB의 member테이블에 저장된 전체 회원을 가져와 화면에 출력해주는 기능
 		//1. DB에서 member테이블에 있는 데이터 가져오기
 		List<MemberDTO> members=new AdminService().selectMemberAll(cPage,numPerpage);
+		
 		//2. DB에서 가져온 데이터 저장(화면출력)
 		request.setAttribute("members", members);
+		
 		//3. 페이지바를 구성
-		// 1)DB에 저장된 전체 데이터의 수를 가져오기
+		// 1)DB에 저장된 전체 데이터의 수를 가져오기 : totalData
 		int totalData=new AdminService().selectMemberCount();
-		// 2)전체 페이지 수 계산하기(*소수점 주의 : 소수점 -> 올림)
-		int totalPage=(int)Math.ceil((double)totalData/numPerpage);//v : 형변환 잘 생각하기
-		// 3) 페이지바 시작번호 계산하기
+		
+		// 2)전체 페이지 수 계산하기(*소수점 주의 : 소수점 -> 올림) : totalPage
+		int totalPage=(int)Math.ceil((double)totalData/numPerpage);//v : 형변환 잘 생각하기, double안하면 마지막 페이지 빠질 수 있음
+		
+		// 3) 페이지바 시작번호 계산하기 : pageBarSize, pageNo, pageEnd
 		int pageBarSize=5;//pageBar에 출력될page번호의 갯수를 저장하는 개수	
 		int pageNo  = ((cPage-1)/pageBarSize)*pageBarSize+1;//v : (cPage-1)/pageBarSize는 분자%분모의 몫 
 		int pageEnd = pageNo+pageBarSize-1;
 		
-		//4. 페이지바를 구성하는 html저장하기
+		//4. 페이지바를 구성하는 html저장하기 : pageBar
 		String pageBar="";//append방식이 더 효율적
 		//이전 표시하기
 		if(pageNo==1) {
@@ -75,16 +120,13 @@ public class MemberListServlet extends HttpServlet {
 			}
 			pageNo++;
 		}
-		
 		//다음 출력
 		if(pageNo>totalPage) {
 			pageBar+="<span>[다음]</span>";
 		}else {
-			pageBar+="<a href='"+request.getRequestURI()+"?cPage="+pageNo+"'>다음</a>";
+			pageBar+="<a href='"+request.getRequestURI()+"?cPage="+pageNo+"'>[다음]</a>";
 		}
-
 		request.setAttribute("pageBar", pageBar);
-		
 		
 		//4.출력할 화면을 선택(이동)
 		request.getRequestDispatcher("/views/admin/manageMember.jsp").forward(request, response);
--- 06_HelloMVC/src/main/webapp/views/admin/manageMember.jsp
+++ 06_HelloMVC/src/main/webapp/views/admin/manageMember.jsp
@@ -4,6 +4,8 @@
 <%
 	List<MemberDTO> members=(List)request.getAttribute("members");	
 %> 
+<!-- css파일 불러오기 필요! -->
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -17,11 +19,66 @@
     
     section#memberList-container table#tbl-member {width:100%; border:1px solid gray; border-collapse:collapse;}
     section#memberList-container table#tbl-member th, table#tbl-member td {border:1px solid gray; padding:10px; }
+    
+    #pageBar a, #pageBar span{text-decoration:none;font-size:14px;margin-left:1%;margin-right:1%;color:green}
+    #pageBar a:hober{background-color:green;}
+    #pageBar span{color:gray;}
+    
+    /* 검색창에 대한 스타일 */
+    div#search-container {margin:0 0 10px 0; padding:3px; 
+    background-color: rgba(0, 188, 212, 0.3);}
+    div#search-userId{display:inline-block;}
+    div#search-userName{display:none;}
+    div#search-gender{display:none;}
+    div#numPerpage-container{float:right;}
+    form#numperPageFrm{display:inline;}
     </style>
 <!--     http://localhost:9090/06_HelloMVC2/admin/memberList.do
     http://localhost:9090/06_HelloMVC2/views/admin/memberlist.jsp -->
     <section id="memberList-container">
         <h2>회원관리</h2>
+        <div id="search-container">
+        	검색타입 : 
+        	<select id="searchType">
+        		<option value="userId" >아이디</option>
+        		<option value="userName" >회원이름</option>
+        		<option value="gender" >성별</option>
+        	</select>
+        	<div id="search-userId">
+        		<form action="<%=request.getContextPath()%>/admin/searchMember">
+        			<input type="hidden" name="searchType" value="userId" >
+        			<input type="text" name="searchKeyword" size="25" 
+        			placeholder="검색할 아이디를 입력하세요" >
+        			<button type="submit">검색</button>
+        		</form>
+        	</div>
+        	<div id="search-userName">
+        		<form action="<%=request.getContextPath()%>/admin/searchMember">
+        			<input type="hidden" name="searchType" value="userName">
+        			<input type="text" name="searchKeyword" size="25" 
+        			placeholder="검색할 이름을 입력하세요">
+        			<button type="submit">검색</button>
+        		</form>
+        	</div>
+        	<div id="search-gender">
+        		<form action="<%=request.getContextPath()%>/admin/searchMember">
+        			<input type="hidden" name="searchType" value="gender">
+        			<label><input type="radio" name="searchKeyword" value="M" >남</label>
+        			<label><input type="radio" name="searchKeyword" value="F" >여</label>
+        			<button type="submit">검색</button>
+        		</form>
+        	</div>
+        </div>
+        <div id="numPerpage-container">
+        	페이지당 회원수 : 
+        	<form id="numPerFrm" action="">
+        		<select name="numPerpage" id="numPerpage">
+        			<option value="3" >3</option>
+        			<option value="5" >5</option>
+        			<option value="10">10</option>
+        		</select>
+        	</form>
+        </div>
         <table id="tbl-member">
             <thead>
                 <tr>
@@ -62,10 +119,19 @@
         </table>
         <div id="pageBar">
         	<%=request.getAttribute("pageBar")%>
-        </div>
-        
-        
+        </div>       
     </section>
+    
+    <script>
+    	$("#searchType").change(e=>{
+	    	/* alert("이벤트발생"); */
+	    	const type=$(e.target).val();
+	    	console.log(type);
+	    	/*x $(e.garget).parent().children().css("display","none"); */
+	    	$(e.target).parent().find("div").css("display","none");
+	    	$("#search-"+type).css("display","inline-block");
+	    });
+    </script>
 </body>
 <%@ include file="/views/common/footer.jsp"%>
 </html>
