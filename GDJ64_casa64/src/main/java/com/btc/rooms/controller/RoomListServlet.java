07e39b5052cc3ebb7df40526764b8088ea40c58a
GDJ <GDJ@DESKTOP-NAPOHR9>
2023-06-21 11:48:28 +0900

    Merge branch 'main' of https://github.com/Muggung/btc-casa64-project.git into sol

--- GDJ64_casa64/src/main/java/com/btc/rooms/controller/RoomListServlet.java
+++ GDJ64_casa64/src/main/java/com/btc/rooms/controller/RoomListServlet.java
@@ -1,16 +1,21 @@
 package com.btc.rooms.controller;
 
 import java.io.IOException;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.btc.rooms.model.service.RoomService;
+import com.btc.rooms.model.vo.Room;
+
 /**
  * Servlet implementation class RoomListServlet
  */
-@WebServlet("/RoomListServlet")
+@WebServlet("/RoomListServlet.do")
 public class RoomListServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -26,8 +31,9 @@ public class RoomListServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		List<Room> rooms=new RoomService().selectAllRoom();
+		request.setAttribute("rooms", rooms);
+		request.getRequestDispatcher("/views/rooms/roomsMain.jsp").forward(request, response);
 	}
 
 	/**
