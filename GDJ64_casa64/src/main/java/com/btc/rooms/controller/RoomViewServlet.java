42204726a37087b25086cad2da991e9a6f76d4b9
GDJ <GDJ@DESKTOP-NAPOHR9>
2023-06-21 10:37:57 +0900

    sol: rooms update

---
+++ GDJ64_casa64/src/main/java/com/btc/rooms/controller/RoomViewServlet.java
@@ -0,0 +1,43 @@
+package com.btc.rooms.controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.btc.rooms.model.service.RoomService;
+import com.btc.rooms.model.vo.Room;
+
+/**
+ * Servlet implementation class RoomViewServlet
+ */
+@WebServlet("/RoomViewServlet.do")
+public class RoomViewServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+   
+    public RoomViewServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		int roomNo=Integer.parseInt(request.getParameter("roomNo"));
+		String type=request.getParameter("type");
+		List<Room> rooms=new RoomService().selectRoomByType(type);
+		request.setAttribute("rooms", rooms);
+		request.getRequestDispatcher("views/rooms/roomsDetail.jsp").forward(request, response);
+	}
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ GDJ64_casa64/src/main/java/com/btc/rooms/model/service/RoomService.java
@@ -0,0 +1,21 @@
+package com.btc.rooms.model.service;
+
+import java.sql.Connection;
+import java.util.List;
+
+import com.btc.rooms.model.dao.RoomDao;
+import com.btc.rooms.model.vo.Room;
+
+import static com.btc.common.JDBCTemplate.*;
+
+public class RoomService {
+	private RoomDao dao=new RoomDao();
+	
+	public List<Room> selectRoomByType(String type){
+		Connection conn=getConnection();
+		List<Room>list=dao.selectRoomByType(conn, type);
+		close(conn);
+		return list;
+	}
+
+}
