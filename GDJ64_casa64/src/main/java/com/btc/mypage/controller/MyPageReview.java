3a0738cf2ef3326da84e30aeb6bd79449c99ef19
DESKTOP-C1T83CR\USER <yooonnara@gmail.com>
2023-06-21 21:43:56 +0900

    nara : mypage 커밋

---
+++ GDJ64_casa64/src/main/java/com/btc/mypage/controller/MyPageReview.java
@@ -0,0 +1,49 @@
+package com.btc.mypage.controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.btc.review.model.service.ReviewService;
+import com.btc.review.model.vo.Reviews;
+
+/**
+ * Servlet implementation class MyPageReview
+ */
+@WebServlet("/mypage/myPageReview")
+public class MyPageReview extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MyPageReview() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		int memberNo= 1;
+		List<Reviews> list = new ReviewService().selectReviewsMypage(memberNo);
+		request.setAttribute("MypageReviewList", list);
+		request.getRequestDispatcher("/views/myPage/myPageReview.jsp").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
