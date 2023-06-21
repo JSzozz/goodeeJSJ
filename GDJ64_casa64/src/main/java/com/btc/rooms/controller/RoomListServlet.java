ab18690a6f16c9ec259b85ea658a7c88d58790c3
GDJ <GDJ@DESKTOP-NAPOHR9>
2023-06-21 11:24:39 +0900

    footer update, roomsList added

--- GDJ64_casa64/src/main/java/com/btc/rooms/controller/RoomListServlet.java
+++ GDJ64_casa64/src/main/java/com/btc/rooms/controller/RoomListServlet.java
@@ -1,40 +1,38 @@
 package com.btc.rooms.controller;
 
 import java.io.IOException;
-import java.util.List;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.btc.rooms.model.service.RoomService;
-import com.btc.rooms.model.vo.Room;
-
 /**
- * Servlet implementation class RoomViewServlet
+ * Servlet implementation class RoomListServlet
  */
-@WebServlet("/RoomViewServlet.do")
-public class RoomViewServlet extends HttpServlet {
+@WebServlet("/RoomListServlet")
+public class RoomListServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-   
-    public RoomViewServlet() {
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public RoomListServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
 
-	
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		/* int roomNo=Integer.parseInt(request.getParameter("roomNo")); */
-		String type=request.getParameter("type");
-		List<Room> rooms=new RoomService().selectRoomByType(type);
-		request.setAttribute("rooms", rooms);
-		request.getRequestDispatcher("/views/rooms/roomsDetail.jsp").forward(request, response);
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
-
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
