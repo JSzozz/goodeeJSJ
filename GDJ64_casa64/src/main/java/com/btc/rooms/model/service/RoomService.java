3b07dad6bfb1c2d458690932672c08bc6040a575
GDJ <GDJ@DESKTOP-NAPOHR9>
2023-06-22 14:15:51 +0900

    sol: header>rooms links updated

--- GDJ64_casa64/src/main/java/com/btc/rooms/model/service/RoomService.java
+++ GDJ64_casa64/src/main/java/com/btc/rooms/model/service/RoomService.java
@@ -11,18 +11,23 @@ import static com.btc.common.JDBCTemplate.*;
 public class RoomService {
 	private RoomDao dao=new RoomDao();
 	
-	public List<Room> selectRoomByType(String type){
-		Connection conn=getConnection();
-		List<Room>list=dao.selectRoomByType(conn, type);
-		close(conn);
-		return list;
-	}
+	/*
+	 * public List<Room> selectRoomByType(String type){ Connection
+	 * conn=getConnection(); List<Room>list=dao.selectRoomByType(conn, type);
+	 * close(conn); return list; }
+	 */
 
 	public List<Room> selectAllRoom() {
 		Connection conn=getConnection();
 		List<Room> list=dao.selectAllRoom(conn);
 		close(conn);
 		return list;
+	}
+
+	public Room viewRoom(int roomNo) {
+		Connection conn=getConnection();
+		Room r=dao.viewRoom(conn,roomNo);
+		return r;
 	}
 
 }
