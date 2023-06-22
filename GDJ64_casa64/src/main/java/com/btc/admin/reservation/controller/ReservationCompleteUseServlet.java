2a2f56d596338383ab2f614f38c92bd6851e7a47
Dongje Lee <mgh0222@naver.com>
2023-06-22 18:45:13 +0900

    Revert "테스트"
    This reverts commit 560dc540668b79490ac47255cc57899847f6cb46.

--- GDJ64_casa64/src/main/java/com/btc/admin/reservation/controller/ReservationCompleteUseServlet.java
+++ GDJ64_casa64/src/main/java/com/btc/admin/reservation/controller/ReservationCompleteUseServlet.java
@@ -2,7 +2,6 @@ package com.btc.admin.reservation.controller;
 
 import java.io.IOException;
 import java.time.LocalDate;
-import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -21,8 +20,6 @@ public class ReservationCompleteUseServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// 오늘 날짜 데이터 저장하기
 		LocalDate now = LocalDate.now();
-		
-		List<Reservation> list = new AdminService().ReservationCompleteUseServlet(now);
 		
 		response.getWriter().print("하이하이");
 	}
