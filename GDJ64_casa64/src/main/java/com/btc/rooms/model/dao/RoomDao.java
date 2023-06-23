f8bb3438029b251cb284c13d3ded2dee0c36b1ca
SOL <GDJ@DESKTOP-NAPOHR9>
2023-06-23 13:21:37 +0900

    sol: roomdao 불필요부분 주석처리

--- GDJ64_casa64/src/main/java/com/btc/rooms/model/dao/RoomDao.java
+++ GDJ64_casa64/src/main/java/com/btc/rooms/model/dao/RoomDao.java
@@ -32,26 +32,26 @@ public class RoomDao {
 				.dateCreated(rs.getDate("date_created")).dateModified(rs.getDate("date_modified"))
 				.roomDescription(rs.getString("room_description")).build();
 	}
-	public List<Room> selectRoomByType(Connection conn, String type){
-		PreparedStatement pstmt=null;
-		ResultSet rs=null;
-		List<Room> list=new ArrayList();
-		try {
-			String query=sql.getProperty("selectRoomByType");
-			query.replace("#", "%"+type+"%");
-			pstmt=conn.prepareStatement(query);
-			rs=pstmt.executeQuery();
-			while(rs.next()) {
-				list.add(getRoom(rs));
-			}
-		}catch(SQLException e) {
-			e.printStackTrace();
-		}finally {
-			close(rs);
-			close(pstmt);
-		}
-		return list;
-	}
+//	public List<Room> selectRoomByType(Connection conn, String type){
+//		PreparedStatement pstmt=null;
+//		ResultSet rs=null;
+//		List<Room> list=new ArrayList();
+//		try {
+//			String query=sql.getProperty("selectRoomByType");
+//			query.replace("#", "%"+type+"%");
+//			pstmt=conn.prepareStatement(query);
+//			rs=pstmt.executeQuery();
+//			while(rs.next()) {
+//				list.add(getRoom(rs));
+//			}
+//		}catch(SQLException e) {
+//			e.printStackTrace();
+//		}finally {
+//			close(rs);
+//			close(pstmt);
+//		}
+//		return list;
+//	}
 	public List<Room> selectAllRoom(Connection conn) {
 		PreparedStatement pstmt=null;
 		ResultSet rs=null;
