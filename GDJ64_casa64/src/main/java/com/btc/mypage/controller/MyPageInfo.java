c6bfc255b7ee68e672fedb18bad4b7d0b3141fc8
DESKTOP-C1T83CR\USER <yooonnara@gmail.com>
2023-06-23 11:49:36 +0900

    nara : 마이페이지 수정

--- GDJ64_casa64/src/main/java/com/btc/mypage/controller/MyPageInfo.java
+++ GDJ64_casa64/src/main/java/com/btc/mypage/controller/MyPageInfo.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class MyPageInfo
  */
-@WebServlet("/mypage/myPageInfo")
+@WebServlet("/myPage/myPageInfo")
 public class MyPageInfo extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
