fe2874f9613aec1b80ce55a1d322d8b8d72a067a
SW <liledim7@gmail.com>
2023-06-28 21:51:46 +0900

    sw 네이버 url 수정

--- GDJ64_casa64/src/main/java/com/btc/member/controller/NaverLoginServlet.java
+++ GDJ64_casa64/src/main/java/com/btc/member/controller/NaverLoginServlet.java
@@ -60,7 +60,7 @@ public class NaverLoginServlet extends HttpServlet {
 		String clientSecret = sql.getProperty("nPw");// 애플리케이션 클라이언트 시크릿값";
 		String code = request.getParameter("code");
 		String state = request.getParameter("state");
-		String redirectURI = URLEncoder.encode("http://localhost:9090/GDJ64_casa64/index.jsp", "UTF-8");
+		String redirectURI = URLEncoder.encode("http://14.36.141.71:10005/GDJ64_casa64_semi/index.jsp", "UTF-8");
 		String apiURL;
 		apiURL = "https://nid.naver.com/oauth2.0/token?grant_type=authorization_code&";
 		apiURL += "client_id=" + clientId;
