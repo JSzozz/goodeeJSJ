b0c1fb48e451ae0b9233ad4028b716cb6485eb8b
jsjyyy02190@gmail.com <tkdwns219@naver.com>
2023-07-11 10:21:17 +0900

    bean등록하기2(어노테이션방식) @Configuration @Bean
    1-1. @Configuration : 클래식 방식으로 bean등록해서 사용하기
    1-2. @Bean :spring에서 사용할 bean을 자바 코드로 등록할 수 있다.
    (xml은 id, class, name 순)
    (@bean은(default) method명, return, 각 build된 속성 순)

--- src/main/java/com/bs/spring/config/BeanTestConfiguration.java
+++ src/main/java/com/bs/spring/config/BeanTestConfiguration.java
@@ -1,8 +1,25 @@
 package com.bs.spring.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.bs.spring.beantest.Animal;
+
 //클래식 방식으로 bean등록해서 사용하기
-//pojo클래스를 configuraion으로 사용할 수 있음 
+//pojo클래스를 configuraion으로 사용할 수 있음
+// -> @Configuration 어노테이션 이용
 
+@Configuration
 public class BeanTestConfiguration {
-
+	//springbeanconfiguration.xml과 동일한 기능
+	
+	//spring에서 사용할 bean을 자바 코드로 등록할 수 있다.
+	//@Bean어노테이션을 이용
+	//메소드선언을 통해 등록함.
+	@Bean
+	public Animal ani() {
+		return Animal.builder().name("킥킥").age(5).height(80).build();
+	}
+	
+	
 }
