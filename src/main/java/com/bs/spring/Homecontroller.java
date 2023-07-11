0d3b44fd777b6767aff4cef9f5c01ef1a85a49e3
jsjyyy02190@gmail.com <tkdwns219@naver.com>
2023-07-11 09:27:26 +0900

    NoUniqueBeanDefinitionException, @Autowired & @Qualifier
    1. (유의)NoUniqueBeanDefinitionException(~autowired)
    - 해소법: 타입을 기준으로 데이터를 집어넣었을 때 타입이 2개니까 둘 중 하나를 없애든가, 둘을 구분해라(필드명과
    <bean>의[id]를 일치하도록하자)
    2.@Autowired & @Qualifier("dog")
    - 중복된 타입이 있는 경우 @Quealifier어노테이션을 이용해서 특정 bren을 선택할 수 있음

--- src/main/java/com/bs/spring/Homecontroller.java
+++ src/main/java/com/bs/spring/Homecontroller.java
@@ -1,6 +1,7 @@
 package com.bs.spring;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,14 +9,22 @@ import com.bs.spring.beantest.Animal;
 
 @Controller
 public class Homecontroller {
-	//springbean으로 등록된 객체는 field에 가져와 사용할 수 있음
+	//@Autowired : springbean으로 등록된 객체는 field에 가져와 사용할 수 있음
+	//@Qualifier : 중복된 타입이 있는 경우 @Quealifier어노테이션을 
+	//             이용해서 특정 bren을 선택할 수 있음
 	@Autowired
-	private Animal a;
+	@Qualifier("dog")
+	private Animal bbo;
+	@Autowired
+	@Qualifier("bbo")
+	private Animal dog;
+
 	
 
 	@RequestMapping("/")
 	public String hone() {
-		System.out.println(a+"(*Homecontroller)");//Animal(name=null, age=0, weight=0.0)(*Homecontroller)
+		System.out.println(bbo+"(*Homecontroller)");//Animal(name=null, age=0, weight=0.0)(*Homecontroller)
+		System.out.println(dog+"(*Homecontroller)");
 		return "index";
 	}
 	
