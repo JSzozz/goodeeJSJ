2d863bea900abf03feea3a282309bf20efd92918
jsjyyy02190@gmail.com <tkdwns219@naver.com>
2023-07-13 14:46:17 +0900

    security 이용한 암호화3(*MemberController.java) : 빈 기입하기
    	@Autowired
    	private BCryptPasswordEncoder passwordEncoder;

--- src/main/java/com/bs/spring/member/controller/MemberController.java
+++ src/main/java/com/bs/spring/member/controller/MemberController.java
@@ -5,6 +5,7 @@ import java.util.Map;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,6 +24,9 @@ public class MemberController {
 
 	@Autowired
 	public MemberService service;
+	
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
 	
 	@RequestMapping("/enrollMember.do")
 	public String enrollMember() {
