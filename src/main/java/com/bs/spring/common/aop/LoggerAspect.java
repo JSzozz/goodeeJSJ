2e389405b58723edbb505b2a247fba3a00e3baf5
jsjyyy02190@gmail.com <tkdwns219@naver.com>
2023-07-14 15:16:17 +0900

    [AOP (Aspect Oriented Programming)] 2. JoinPoint 객체(*LoggerAspect.java)
    1. com.bs.spring.common.aop 생성
    2. LoggerAspect.java 생성
    3. JoinPoint jp 객체 이용

---
+++ src/main/java/com/bs/spring/common/aop/LoggerAspect.java
@@ -0,0 +1,14 @@
+package com.bs.spring.common.aop;
+
+import org.aspectj.lang.JoinPoint;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class LoggerAspect {
+
+	public  void loggerBefor(JoinPoint jp) {
+	log.debug("-----------AOP loogerBefor start-----------");
+	log.debug("-----------AOP loogerBefor end-----------");
+	}
+}
