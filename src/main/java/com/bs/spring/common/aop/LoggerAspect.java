517d21b282509e9d74d34e09e591d8c82e9e1f9f
jsjyyy02190@gmail.com <tkdwns219@naver.com>
2023-07-14 16:17:12 +0900

    [AOP] 8.사용예시- loggerBefore(*LoggerAspect.java)
    1. 메소드가 실행될때 전달되는 매개변수의 인수값을 가져오기
    
    	public  void loggerBefore(JoinPoint jp) {
    	log.debug("-----------AOP loogerBefore start-----------");
    	Signature sig=jp.getSignature();
    	//import org.aspectj.lang.Signature;
    	log.debug(sig.getDeclaringTypeName()+" "+sig.getName());
    
    	Object[] arg=jp.getArgs();//메소드가 실행될때 전달되는 매개변수의 인수값을 가져옴
    	if(arg!=null) {
    		for(Object o : arg) {
    			log.debug("{}",o);
    			if(o instanceof Map) {//log 속에 넣고싶은 값 추가하기
    				((Map) o).put("test", "째훈째훈");
    			}
    		}
    	}
    	log.debug("-----------AOP loogerBefore end-----------");
    	}
    
    출력 :
    [2023-07-14 16:18:39] DEBUG :
    [com.bs.spring.common.aop.LoggerAspect.loggerBefore(LoggerAspect.java:14)]
    - -----------AOP loogerBefore start-----------
    [2023-07-14 16:18:39] DEBUG :
    [com.bs.spring.common.aop.LoggerAspect.loggerBefore(LoggerAspect.java:17)]
    - com.bs.spring.member.service.MemberService selectMemberById
    [2023-07-14 16:18:39] DEBUG :
    [com.bs.spring.common.aop.LoggerAspect.loggerBefore(LoggerAspect.java:22)]
    - {userId=admin, password=1234}
    [2023-07-14 16:18:39] DEBUG :
    [com.bs.spring.common.aop.LoggerAspect.loggerBefore(LoggerAspect.java:28)]
    - -----------AOP loogerBefore end-----------
    [2023-07-14 16:18:39] DEBUG :
    [com.bs.spring.common.aop.LoggerAspect.loggerBefore(LoggerAspect.java:14)]
    - -----------AOP loogerBefore start-----------
    [2023-07-14 16:18:39] DEBUG :
    [com.bs.spring.common.aop.LoggerAspect.loggerBefore(LoggerAspect.java:17)]
    - com.bs.spring.member.dao.MemberDao selectMemberById
    [2023-07-14 16:18:39] DEBUG :
    [com.bs.spring.common.aop.LoggerAspect.loggerBefore(LoggerAspect.java:22)]
    - org.mybatis.spring.SqlSessionTemplate@53971405
    [2023-07-14 16:18:39] DEBUG :
    [com.bs.spring.common.aop.LoggerAspect.loggerBefore(LoggerAspect.java:22)]
    - {userId=admin, password=1234, test=째훈째훈}
    [2023-07-14 16:18:39] DEBUG :
    [com.bs.spring.common.aop.LoggerAspect.loggerBefore(LoggerAspect.java:28)]
    - -----------AOP loogerBefore end-----------

--- src/main/java/com/bs/spring/common/aop/LoggerAspect.java
+++ src/main/java/com/bs/spring/common/aop/LoggerAspect.java
@@ -1,5 +1,7 @@
 package com.bs.spring.common.aop;
 
+import java.util.Map;
+
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.Signature;
 
@@ -9,19 +11,29 @@ import lombok.extern.slf4j.Slf4j;
 public class LoggerAspect {
 
 	public  void loggerBefore(JoinPoint jp) {
-	log.debug("-----------AOP loogerBefore start-----------");
-	Signature sig=jp.getSignature();
-	//import org.aspectj.lang.Signature;
-	log.debug(sig.getDeclaringTypeName()+" "+sig.getName());
-	log.debug("-----------AOP loogerBefore end-----------");
+		log.debug("-----------AOP loogerBefore start-----------");
+		Signature sig=jp.getSignature();
+		//import org.aspectj.lang.Signature;
+		log.debug(sig.getDeclaringTypeName()+" "+sig.getName());
+		
+		Object[] arg=jp.getArgs();//메소드가 실행될때 전달되는 매개변수의 인수값을 가져옴
+		if(arg!=null) {
+			for(Object o : arg) {
+				log.debug("{}",o);
+				if(o instanceof Map) {//log 속에 넣고싶은 값 추가하기
+					((Map) o).put("test", "째훈째훈");
+				}
+			}
+		}
+		log.debug("-----------AOP loogerBefore end-----------");
 	}
 	
 	public void loggerAfter(JoinPoint jp) {
-		log.debug("----- AOP loggerAfter -----");
+		log.debug("----- AOP loggerAfter start -----");
 		Signature sig=jp.getSignature();
 		
 		log.debug(sig.getDeclaringTypeName()+" "+sig.getName());
 				
-		log.debug("----------------------------");
+		log.debug("----- AOP loggerAfter end -----");
 	}
 }
