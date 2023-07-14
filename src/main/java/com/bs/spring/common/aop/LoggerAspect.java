726d6440f92e2f34d41d40331164f5933cc42e8d
jsjyyy02190@gmail.com <tkdwns219@naver.com>
2023-07-14 15:45:24 +0900

    실수 처리(*web.xml)
    파일위치 실수 :
    appServlet > aop-context.xml
    
    기입 실수 :
    (x)
    	<!-- The definition of the Root Spring Container shared by all Servlets
    and Filters -->
    	<context-param>
    		<param-name>contextConfigLocation</param-name>
    		<param-value>
    			/WEB-INF/spring/root-context.xml
    			/WEB-INF/spring/security-context.xml
    		</param-value>
    	</context-param>
    
    (o)
    	<!-- The definition of the Root Spring Container shared by all Servlets
    and Filters -->
    	<context-param>
    		<param-name>contextConfigLocation</param-name>
    		<param-value>
    			/WEB-INF/spring/root-context.xml
    			/WEB-INF/spring/security-context.xml
    		</param-value>
    	</context-param>

--- src/main/java/com/bs/spring/common/aop/LoggerAspect.java
+++ src/main/java/com/bs/spring/common/aop/LoggerAspect.java
@@ -1,6 +1,7 @@
 package com.bs.spring.common.aop;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.Signature;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -8,7 +9,10 @@ import lombok.extern.slf4j.Slf4j;
 public class LoggerAspect {
 
 	public  void loggerBefore(JoinPoint jp) {
-	log.debug("-----------AOP loogerBefor start-----------");
-	log.debug("-----------AOP loogerBefor end-----------");
+	log.debug("-----------AOP loogerBefore start-----------");
+	Signature sig=jp.getSignature();
+	//import org.aspectj.lang.Signature;
+	log.debug(sig.getDeclaringTypeName()+"\n"+sig.getName());
+	log.debug("-----------AOP loogerBefore end-----------");
 	}
 }
