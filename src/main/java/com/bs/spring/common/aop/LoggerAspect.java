e73280b959944463de0ef0ab59b890f92bcd0941
jsjyyy02190@gmail.com <tkdwns219@naver.com>
2023-07-14 15:31:12 +0900

    [AOP (Aspect Oriented Programming)] 3. xml방식으로 aop적용하기(*aop-context.xml)
    1. xml방식으로 aop적용하기
    	<bean id="loggerAspect" class="com.bs.spring.common.aop.LoggerAspect"/>
    
    2.
    	<aop:config>
    		<aop:aspect ref="loggerAspect">
    			<aop:pointcut expression="execution(* com.bs.spring.memo..*(..))"
    id="logPointcut"/>
    			<aop:before method="loggerBefore" pointcut-ref="logPointcut"/>
    		</aop:aspect>
    	</aop:config>
    
    설명.
    spring.memo..*(..)
    - 첫'..'은 이하의 모든 클래스 의미
    -'*(..)'은 클래스 내부의 메소드부분 표현식임
    --ㄴ'*'은 모든 접근제한자 + 반환형 의미
    --ㄴ'(..)'는 	모든 매개변수를 의미

--- src/main/java/com/bs/spring/common/aop/LoggerAspect.java
+++ src/main/java/com/bs/spring/common/aop/LoggerAspect.java
@@ -7,7 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class LoggerAspect {
 
-	public  void loggerBefor(JoinPoint jp) {
+	public  void loggerBefore(JoinPoint jp) {
 	log.debug("-----------AOP loogerBefor start-----------");
 	log.debug("-----------AOP loogerBefor end-----------");
 	}
