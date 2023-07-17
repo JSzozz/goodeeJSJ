c716c9d99e1815d9658781d3708cfe7f70d385c4
jsjyyy02190@gmail.com <tkdwns219@naver.com>
2023-07-17 09:27:43 +0900

    [AOP] 9-1.사용예시2[@Pointcut_'3.'유의](*AnnoLoggerAspect.java)(*aop-context.xml)
    1. @Slf4j(로그 API), @Component(빈 등록), @Aspect
    (aspect 역할 등록)
    	@Pointcut("within(com.bs.spring.member.*)")
    	public void loggerTest() {}
    참조 : https://docs.spring.io/spring-framework/docs/5.3.29/reference/html/core.html#aop
    
    2. @Pointcut, @Before
    	//advisor 설정
    	@Before("loggerTest()")
    	public void loggerBefor(JoinPoint jp){//Joinpoint (x)
    		log.debug("============== annotation aop start========");
    		Signature sig=jp.getSignature();
    		log.debug(sig.getDeclaringTypeName()+" "+sig.getName());
    		log.debug("============== annotation aop end========");
    	}
    
    3.(필수) aspect등록을 위한 필수(*aop-context.xml)
    	<!-- annotation방식으로 AOP 적용하기 : @aspect 사용을 위한 필수! -->
    	<aop:aspectj-autoproxy></aop:aspectj-autoproxy>
    
    4. 출력문_
    [2023-07-17 09:25:48] DEBUG :
    [com.bs.spring.common.aop.AnnoLoggerAspect.loggerBefor(AnnoLoggerAspect.java:24)]
    - ============== annotation aop start========
    [2023-07-17 09:25:49] DEBUG :
    [com.bs.spring.common.aop.AnnoLoggerAspect.loggerBefor(AnnoLoggerAspect.java:26)]
    - com.bs.spring.member.controller.MemberController login
    [2023-07-17 09:25:49] DEBUG :
    [com.bs.spring.common.aop.AnnoLoggerAspect.loggerBefor(AnnoLoggerAspect.java:27)]
    - ============== annotation aop end========
    [2023-07-17 09:25:49] DEBUG :
    [com.bs.spring.common.aop.AnnoLoggerAspect.loggerBefor(AnnoLoggerAspect.java:24)]
    - ============== annotation aop start========
    [2023-07-17 09:25:49] DEBUG :
    [com.bs.spring.common.aop.AnnoLoggerAspect.loggerBefor(AnnoLoggerAspect.java:26)]
    - com.bs.spring.member.service.MemberService selectMemberById
    [2023-07-17 09:25:49] DEBUG :
    [com.bs.spring.common.aop.AnnoLoggerAspect.loggerBefor(AnnoLoggerAspect.java:27)]
    - ============== annotation aop end========
    [2023-07-17 09:25:49] DEBUG :
    [com.bs.spring.common.aop.AnnoLoggerAspect.loggerBefor(AnnoLoggerAspect.java:24)]
    - ============== annotation aop start========
    [2023-07-17 09:25:49] DEBUG :
    [com.bs.spring.common.aop.AnnoLoggerAspect.loggerBefor(AnnoLoggerAspect.java:26)]
    - com.bs.spring.member.dao.MemberDao selectMemberById
    [2023-07-17 09:25:49] DEBUG :
    [com.bs.spring.common.aop.AnnoLoggerAspect.loggerBefor(AnnoLoggerAspect.java:27)]
    - ============== annotation aop end========

---
+++ src/main/java/com/bs/spring/common/aop/AnnoLoggerAspect.java
@@ -0,0 +1,30 @@
+package com.bs.spring.common.aop;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.Signature;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+@Aspect
+public class AnnoLoggerAspect {
+
+	//pointcut 설정
+	@Pointcut("within(com.bs.spring.member..*)")
+	public void loggerTest() {}
+
+	//advisor 설정
+	@Before("loggerTest()")
+	public void loggerBefor(JoinPoint jp){//Joinpoint (x)
+		log.debug("============== annotation aop start========");
+		Signature sig=jp.getSignature();
+		log.debug(sig.getDeclaringTypeName()+" "+sig.getName());
+		log.debug("============== annotation aop end========");
+	}
+	
+}
