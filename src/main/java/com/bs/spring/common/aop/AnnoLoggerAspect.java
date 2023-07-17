1301082f6cc82159ee240894cfddbde5fe6ba874
jsjyyy02190@gmail.com <tkdwns219@naver.com>
2023-07-17 09:41:28 +0900

    [AOP] 9-1.사용예시3[@Around](*AnnoLoggerAspect.java)
    1. @Around
    //메소드 실행 전,후에 특정 로직을 실행할때 사용
    
    2. proceed()
    //proceed()메소드가 호출한 다음 라인은 후처리, 그전 라인은 전처리
    //proceed()메소드는 Object를 반환한다.
    
    3.(추가) 메소드 실행시간 체크[StopWatch]
    		StopWatch stop=new StopWatch();
    		stop.start();
    		( --- proceed() --- )
    		stop.stop();
    		log.debug("실행시간 : " + stop.getTotalTimeMillis()+"ms");
    
    [code]
    	//메소드 실행 전,후에 특정 로직을 실행할때 사용
    	@Around("execution(* com.bs.spring..*DaoImpl.*(..))")
    	public Object daoLogger(ProceedingJoinPoint join) throws Throwable{
    		//전,후 로직은 한번에 설정할 수 있다....
    		//전, 후를 구분하는 구문은 ProceedingJoinPoint클래스가 제공하는 proceed()메소드를 이용
    		//proceed()메소드가 호출한 다음 라인은 후처리, 그전 라인은 전처리
    		//proceed()메소드는 Object를 반환한다.
    		//메소드 실행시간 체크하기
    		StopWatch stop=new StopWatch();
    		stop.start();
    		log.debug("=========== around logger dao before
    start===============");
    		log.debug("------- 전처리 내용 구현 --------------");
    		log.debug("=========== around logger dao before end===============");
    		Object obj=join.proceed();
    		stop.stop();
    		log.debug("=========== around logger dao  after
    start===============");
    		log.debug("------- 후처리 내용 구현 --------------");
    		log.debug("실행시간 : " + stop.getTotalTimeMillis()+"ms");
    		log.debug("=========== around logger dao  after end===============");
    		return obj;
    	}

--- src/main/java/com/bs/spring/common/aop/AnnoLoggerAspect.java
+++ src/main/java/com/bs/spring/common/aop/AnnoLoggerAspect.java
@@ -1,12 +1,15 @@
 package com.bs.spring.common.aop;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.Signature;
 import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StopWatch;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -32,12 +35,32 @@ public class AnnoLoggerAspect {
 	public void memoLogger() {}
 	
 	@After("memoLogger()")
-	public void afterLogger(JoinPoint jp) {
-		log.debug("======= annotation aop after ========");
+	public void afterLogger(JoinPoint jp){//Joinpoint (x)
+		log.debug("============== annotation aop after start========");
 		Signature sig=jp.getSignature();
 		log.debug(sig.getDeclaringTypeName()+" "+sig.getName());
-		log.debug("============== aop after ==================");
+		log.debug("============== annotation aop after end========");
 	}
 	
-	
+	//메소드 실행 전,후에 특정 로직을 실행할때 사용
+	@Around("execution(* com.bs.spring..*DaoImpl.*(..))")
+	public Object daoLogger(ProceedingJoinPoint join) throws Throwable{
+		//전,후 로직은 한번에 설정할 수 있다....
+		//전, 후를 구분하는 구문은 ProceedingJoinPoint클래스가 제공하는 proceed()메소드를 이용
+		//proceed()메소드가 호출한 다음 라인은 후처리, 그전 라인은 전처리
+		//proceed()메소드는 Object를 반환한다.
+		//메소드 실행시간 체크하기
+		StopWatch stop=new StopWatch();
+		stop.start();
+		log.debug("=========== around logger dao before start===============");
+		log.debug("------- 전처리 내용 구현 --------------");
+		log.debug("=========== around logger dao before end===============");
+		Object obj=join.proceed();
+		stop.stop();
+		log.debug("=========== around logger dao  after start===============");
+		log.debug("------- 후처리 내용 구현 --------------");
+		log.debug("실행시간 : " + stop.getTotalTimeMillis()+"ms");
+		log.debug("=========== around logger dao  after end===============");
+		return obj;
+	}
 }
