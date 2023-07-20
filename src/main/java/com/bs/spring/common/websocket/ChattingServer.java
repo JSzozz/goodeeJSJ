9926ed3e41827e71d1fca5d4d145fa62c296aff7
jsjyyy02190@gmail.com <tkdwns219@naver.com>
2023-07-20 11:44:41 +0900

    [spring websocket]9.값 가져오기(*ChattingServer.java)(*ChattingMessage.java)
    1. 등록한 빈 임포트
    	private ObjectMapper mapper;
    
    2. (*ChattingMessage.java)객체 클래스 생성
    package com.bs.spring.common.websocket;
    
    @Data
    @AllArgsConstructor
    @NoArgsConstructor
    @Builder
    public class ChattingMessage {
    	private String type;
    	private String sender;
    	private String reciever;
    	private String msg;
    	private String room;
    }
    
    3. 생성한 객체로 클라이언트 메세지 받기
    	@Override
    	protected void handleTextMessage(WebSocketSession session, TextMessage
    message) throws Exception {
    		log.info("{}",message);
    		log.info(message.getPayload());//클라이언트가 보낸 데이터
    		ChattingMessage
    msg=mapper.readValue(message.getPayload(),ChattingMessage.class);
    		System.out.println(msg);
    	}

--- src/main/java/com/bs/spring/common/websocket/ChattingServer.java
+++ src/main/java/com/bs/spring/common/websocket/ChattingServer.java
@@ -8,6 +8,8 @@ import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import lombok.extern.slf4j.Slf4j;
 
 
@@ -15,7 +17,8 @@ import lombok.extern.slf4j.Slf4j;
 public class ChattingServer extends TextWebSocketHandler{
 	//key userId, session은 전달한 값을 저장
 	private Map<String, WebSocketSession> clients=new HashMap();
-	
+	private ObjectMapper mapper;
+
 	@Override
 	public void afterConnectionEstablished(WebSocketSession session) throws Exception {
 		log.info("채팅서버 입장");
@@ -26,7 +29,9 @@ public class ChattingServer extends TextWebSocketHandler{
 	protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
 		log.info("{}",message);
 		log.info(message.getPayload());//클라이언트가 보낸 데이터
-	
+		ChattingMessage msg=mapper.readValue(message.getPayload(),ChattingMessage.class);
+		System.out.println(msg);
+
 	}
 
 	@Override
