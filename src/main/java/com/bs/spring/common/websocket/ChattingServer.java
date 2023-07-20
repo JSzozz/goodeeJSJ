918eb9f080c4b06d2b6b5c539578161756fefae2
jsjyyy02190@gmail.com <tkdwns219@naver.com>
2023-07-20 12:02:45 +0900

    [spring websocket]10.접속(자) 처리[open, msg, system..](*ChattingServer.java)
    1-0. '9.값 가져오기 - 1. 등록한 빈 임포트'에서 맨 밑 부분 빠졌었음. 오류 발견 후 수정 기록
    	//key userId, session은 전달된 값을 저장
    	private Map<String,WebSocketSession> clients=new HashMap();
    
    	private ObjectMapper mapper;
    
    	public ChattingServer(ObjectMapper mapper) {
    		this.mapper=mapper;
    	}
    
    1-1. handleTextMessage
    	@Override
    	protected void handleTextMessage(WebSocketSession session, TextMessage
    message) throws Exception {
    		log.info("{}",message);
    		log.info(message.getPayload());//클라이언트가 보낸 데이터
    		ChattingMessage
    msg=mapper.readValue(message.getPayload(),ChattingMessage.class);
    		System.out.println(msg);
    		switch(msg.getType()) {
    		case "open" : addClient(session,msg.getSender());break;
    		case "msg" : sendMessage(msg);break;
    //		case "system" :
    		}
    	}
    
    1-2. addClient
    	private void addClient(WebSocketSession session,String sender) {
    		clients.put(sender, session);
    		log.info("현재접속자 : "+clients.size());
    
    	}
    
    1-3. sendMessage
    	private void sendMessage(ChattingMessage msg) {
    		Set<Map.Entry<String,WebSocketSession>>
    clients=this.clients.entrySet();
    		try {
    			if(msg.getReciever().isBlank()) {
    				//전체전송
    				for(Map.Entry<String,WebSocketSession> client:clients) {
    					String userId=client.getKey();
    					client.getValue().sendMessage(
    							new TextMessage(mapper.writeValueAsString(msg))
    							);
    				}
    			}else{
    				for(Map.Entry<String,WebSocketSession> client:clients) {
    					String userId=client.getKey();
    					if(userId.equals(msg.getReciever())||userId.equals(msg.getSender()))
    {
    						client.getValue().sendMessage(
    								new TextMessage(mapper.writeValueAsString(msg))
    								);
    					}
    				}
    			}
    		}catch(IOException e) {
    			e.printStackTrace();
    		}
    	}

--- src/main/java/com/bs/spring/common/websocket/ChattingServer.java
+++ src/main/java/com/bs/spring/common/websocket/ChattingServer.java
@@ -1,7 +1,9 @@
 package com.bs.spring.common.websocket;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
@@ -13,30 +15,73 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 
 
-@Slf4j 
+@Slf4j
 public class ChattingServer extends TextWebSocketHandler{
-	//key userId, session은 전달한 값을 저장
-	private Map<String, WebSocketSession> clients=new HashMap();
+	
+	//key userId, session은 전달된 값을 저장
+	private Map<String,WebSocketSession> clients=new HashMap();
+	
 	private ObjectMapper mapper;
-
+	
+	public ChattingServer(ObjectMapper mapper) {
+		this.mapper=mapper;
+	}
+	
 	@Override
 	public void afterConnectionEstablished(WebSocketSession session) throws Exception {
 		log.info("채팅서버 입장");
 		log.info(session.getId()+" "+session.getRemoteAddress());
+		
 	}
 
 	@Override
 	protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+		
 		log.info("{}",message);
 		log.info(message.getPayload());//클라이언트가 보낸 데이터
 		ChattingMessage msg=mapper.readValue(message.getPayload(),ChattingMessage.class);
 		System.out.println(msg);
-
+		switch(msg.getType()) {
+			case "open" : addClient(session,msg.getSender());break;
+			case "msg" : sendMessage(msg);break;
+//			case "system" :  
+		}	
+		
 	}
-
+	private void addClient(WebSocketSession session,String sender) {
+		clients.put(sender, session);
+		log.info("현재접속자 : "+clients.size());
+	}
+	
+	private void sendMessage(ChattingMessage msg) {
+		Set<Map.Entry<String,WebSocketSession>> clients=this.clients.entrySet();
+		try {
+			if(msg.getReciever().isBlank()) {
+				//전체전송
+				for(Map.Entry<String,WebSocketSession> client:clients) {
+					String userId=client.getKey();
+					client.getValue().sendMessage(
+							new TextMessage(mapper.writeValueAsString(msg))
+							);
+				}
+			}else{
+				for(Map.Entry<String,WebSocketSession> client:clients) {
+					String userId=client.getKey();
+					if(userId.equals(msg.getReciever())||userId.equals(msg.getSender())) {
+						client.getValue().sendMessage(
+								new TextMessage(mapper.writeValueAsString(msg))
+								);
+					}
+				}
+			}
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
 	@Override
 	public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
 	
-	
 	}
+	
 }
