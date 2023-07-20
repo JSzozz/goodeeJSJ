3d82c8368da79ab85c895433f9013f4ff8ed8041
jsjyyy02190@gmail.com <tkdwns219@naver.com>
2023-07-20 16:06:54 +0900

    첫 커밋

---
+++ src/test/java/com/bs/boot/testboot/TestbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bs.boot.testboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
