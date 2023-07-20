6ae5867ff13eef349125c135b760a41f36c089d0
jsjyyy02190@gmail.com <tkdwns219@naver.com>
2023-07-20 10:34:06 +0900

    [REST API, RESTFul]2.기본이해[ResponseEntity]-에러(*RestMemberController.java)
    1.
    	@GetMapping
    	public ResponseEntity selectMemberAll(){
    		List<Member> members = service.selectMemberAll();
    		ResponseEntity <List<Member>> response = ResponseEntity.ok(members);
    				// new ResponseEntity(members, HttpStatus.BAD_REQUEST);
    
    		return response;
    	}

--- src/main/java/com/bs/spring/member/controller/RestMemberController.java
+++ src/main/java/com/bs/spring/member/controller/RestMemberController.java
@@ -3,16 +3,20 @@ package com.bs.spring.member.controller;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bs.spring.member.model.dto.Member;
 import com.bs.spring.member.service.MemberService;
 
 @RestController
+@RequestMapping("/member")
 public class RestMemberController {
 	
 	// REST API, RESTFul 예)
@@ -29,8 +33,12 @@ public class RestMemberController {
 	}
 	
 	@GetMapping
-	public List<Member> selectMemberAll(){
-		return service.selectMemberAll();
+	public ResponseEntity selectMemberAll(){
+		List<Member> members = service.selectMemberAll();
+		ResponseEntity <List<Member>> response = ResponseEntity.ok(members); 
+				// new ResponseEntity(members, HttpStatus.BAD_REQUEST);
+		
+		return response;
 	}
 	
 	@GetMapping("/{id}")
