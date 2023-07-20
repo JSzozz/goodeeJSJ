97a0d99239947cefb0ad30514f3a20f7c6349eae
jsjyyy02190@gmail.com <tkdwns219@naver.com>
2023-07-20 16:21:07 +0900

    [helloBoot]0.셋팅

---
+++ src/test/java/com/bs/boot/first/HelloBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bs.boot.first;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
