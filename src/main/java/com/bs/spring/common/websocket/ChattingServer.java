d8e47fea388cbcaa3e0049afd6dabb8aae23e8ff
jsjyyy02190@gmail.com <tkdwns219@naver.com>
2023-07-20 11:03:34 +0900

    [spring websocket]4.백앤드(*ChattingServer.java)(*WebsocketController.java)
    0.
    com.bs.spring.common.websocket 생성
    
    1. 기본 골격 생성 (매소드 생성 단축키 있었는데 못들음, 3개 클릭했음)(*ChattingServer.java)
    @Slf4j
    public class ChattingServer extends TextWebSocketHandler{
    
    	@Override
    	public void afterConnectionEstablished(WebSocketSession session) throws
    Exception {
    		log.info("채팅서버 입장");
    		log.info(session.getId()+" "+session.getRemoteAddress());
    	}
    
    	@Override
    	protected void handleTextMessage(WebSocketSession session, TextMessage
    message) throws Exception {
    
    	}
    
    	@Override
    	public void afterConnectionClosed(WebSocketSession session, CloseStatus
    status) throws Exception {
    
    	}
    }
    
    2. (*WebsocketController.java)
    @Controller
    public class WebsocketController {
    	@RequestMapping("/chattingpage")
    	public String chattingPage() {
    		return "chatting/chat";
    	}
    
    }

--- src/main/java/com/bs/spring/common/websocket/ChattingServer.java
+++ src/main/java/com/bs/spring/common/websocket/ChattingServer.java
@@ -1,8 +1,31 @@
-package com.bs.spring.common;
+package com.bs.spring.common.websocket;
+
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 import lombok.extern.slf4j.Slf4j;
 
-@Slf4j
-public class ChattingServer {
 
+@Slf4j
+public class ChattingServer extends TextWebSocketHandler{
+	
+	@Override
+	public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+		log.info("채팅서버 입장");
+		log.info(session.getId()+" "+session.getRemoteAddress());
+	}
+
+	@Override
+	protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+
+	
+	}
+
+	@Override
+	public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+	
+	
+	}
 }
