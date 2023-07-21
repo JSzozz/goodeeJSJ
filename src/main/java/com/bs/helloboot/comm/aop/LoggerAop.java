603bc3c2e5a2ffe201e7bbe16c80811f9a5168e5
jsjyyy02190@gmail.com <tkdwns219@naver.com>
2023-07-21 11:51:17 +0900

    [helloBoot]20. AOP(*pom.xml)(*LoggerAop.java)
    (*pom.xml)
    pom.xml > spring > add starters : 여기 읎다..
    1. aspectj DI 직접등록(by MVN) (*pom.xml)
    		<!-- AOP 적용하기 -->
    		<dependency>
    		    <groupId>org.springframework.boot</groupId>
    		    <artifactId>spring-boot-starter-aop</artifactId>
    		</dependency>
    
    2.(*LoggerAop.java)
    @Aspect
    @Component
    @Slf4j
    public class LoggerAop {
    
    	@Before("execution(* com.bs.helloboot..*(..))")
    	public void logger(JoinPoint jp) {
    		Signature sig=jp.getSignature();
    		//import org.aspectj.lang.Signature;
    
    		log.debug("===== aop before =====");
    		log.debug(sig.getDeclaringTypeName()+" "+sig.getName());
    		log.debug("===== aop after =====");
    	}
    }
    3. 출력문
    [2m2023-07-21 11:50:17.199[0;39m [32mDEBUG[0;39m [35m20728[0;39m
    [2m---[0;39m [2m[  restartedMain][0;39m
    [36mcom.bs.helloboot.comm.aop.LoggerAop     [0;39m [2m:[0;39m =====
    aop before =====
    [2m2023-07-21 11:50:17.200[0;39m [32mDEBUG[0;39m [35m20728[0;39m
    [2m---[0;39m [2m[  restartedMain][0;39m
    [36mcom.bs.helloboot.comm.aop.LoggerAop     [0;39m [2m:[0;39m
    com.bs.helloboot.config.MyWebMvcConfigration addInterceptors
    [2m2023-07-21 11:50:17.200[0;39m [32mDEBUG[0;39m [35m20728[0;39m
    [2m---[0;39m [2m[  restartedMain][0;39m
    [36mcom.bs.helloboot.comm.aop.LoggerAop     [0;39m [2m:[0;39m =====
    aop after =====
    [2m2023-07-21 11:50:17.209[0;39m [32mDEBUG[0;39m [35m20728[0;39m
    [2m---[0;39m [2m[  restartedMain][0;39m
    [36mcom.bs.helloboot.comm.aop.LoggerAop     [0;39m [2m:[0;39m =====
    aop before =====
    [2m2023-07-21 11:50:17.209[0;39m [32mDEBUG[0;39m [35m20728[0;39m
    [2m---[0;39m [2m[  restartedMain][0;39m
    [36mcom.bs.helloboot.comm.aop.LoggerAop     [0;39m [2m:[0;39m
    com.bs.helloboot.config.MyWebMvcConfigration addCorsMappings
    [2m2023-07-21 11:50:17.209[0;39m [32mDEBUG[0;39m [35m20728[0;39m
    [2m---[0;39m [2m[  restartedMain][0;39m
    [36mcom.bs.helloboot.comm.aop.LoggerAop     [0;39m [2m:[0;39m =====
    aop after =====
    [2m2023-07-21 11:50:17.262[0;39m [32mDEBUG[0;39m [35m20728[0;39m
    [2m---[0;39m [2m[  restartedMain][0;39m
    [36mcom.bs.helloboot.comm.aop.LoggerAop     [0;39m [2m:[0;39m =====
    aop before =====
    [2m2023-07-21 11:50:17.263[0;39m [32mDEBUG[0;39m [35m20728[0;39m
    [2m---[0;39m [2m[  restartedMain][0;39m
    [36mcom.bs.helloboot.comm.aop.LoggerAop     [0;39m [2m:[0;39m
    com.bs.helloboot.config.MyWebMvcConfigration addViewControllers
    [2m2023-07-21 11:50:17.263[0;39m [32mDEBUG[0;39m [35m20728[0;39m
    [2m---[0;39m [2m[  restartedMain][0;39m
    [36mcom.bs.helloboot.comm.aop.LoggerAop     [0;39m [2m:[0;39m =====
    aop after =====

---
+++ src/main/java/com/bs/helloboot/comm/aop/LoggerAop.java
@@ -0,0 +1,25 @@
+package com.bs.helloboot.comm.aop;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.Signature;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Aspect
+@Component
+@Slf4j
+public class LoggerAop {
+
+	@Before("execution(* com.bs.helloboot..*(..))")
+	public void logger(JoinPoint jp) {
+		Signature sig=jp.getSignature();
+		//import org.aspectj.lang.Signature;
+		
+		log.debug("===== aop before =====");
+		log.debug(sig.getDeclaringTypeName()+" "+sig.getName());
+		log.debug("===== aop after =====");
+	}
+}
