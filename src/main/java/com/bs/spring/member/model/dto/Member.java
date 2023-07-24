fda11cefbe449872a1f46a4adfa8e5cf02a5e2d5
jsjyyy02190@gmail.com <tkdwns219@naver.com>
2023-07-24 12:41:08 +0900

    [security]15.loginService3 (*Member.java)
    유의 : userName은 security의 ID 키값이므로 변수명 작성 시 유의 필요!
    //	private	String userName;
    	private	String name;
    
    1. 메소드 처리함으로써 loginMember 리턴값 받기 가능해짐
    	@Override
    	public Collection<? extends GrantedAuthority> getAuthorities() {
    		//로그인한 사용자의 권한을 설정하는 메소드
    		List<GrantedAuthority> auth=new ArrayList();
    		auth.add(new SimpleGrantedAuthority("ROLE_USER"));
    		if(userId.equals("admin")) {
    			auth.add(new SimpleGrantedAuthority("ROLE_ADMIN"));
    		}
    		return auth;
    	}
    
    	@Override
    	public String getUsername() {
    		// TODO Auto-generated method stub
    		return null;
    	}
    
    	@Override
    	public boolean isAccountNonExpired() {
    		// TODO Auto-generated method stub
    		return true;
    	}
    
    	@Override
    	public boolean isAccountNonLocked() {
    		// TODO Auto-generated method stub
    		return true;
    	}
    
    	@Override
    	public boolean isCredentialsNonExpired() {
    		// TODO Auto-generated method stub
    		return true;
    	}
    
    	@Override
    	public boolean isEnabled() {
    		// TODO Auto-generated method stub
    		return true;
    	}

--- src/main/java/com/bs/spring/member/model/dto/Member.java
+++ src/main/java/com/bs/spring/member/model/dto/Member.java
@@ -1,14 +1,20 @@
 package com.bs.spring.member.model.dto;
 
 import java.sql.Date;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.Past;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -19,7 +25,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Builder
 @NoArgsConstructor
-public class Member {
+public class Member implements UserDetails {
 	
 	@NotEmpty(message = "아이디를 입력해주세요(4글자 이상)")
 	@Size(min=4)
@@ -30,7 +36,8 @@ public class Member {
 			message ="비밀번호를 입력해주세요(영대문자, 영소문자, 특수기호~!@#$%^&*()를 포함하고 8자 이상으로 작성해주세요)"  )
 	private String password;
 	
-	private	String userName;
+//	private	String userName;
+	private	String name;
 	
 	private	String gender;
 	
@@ -49,4 +56,46 @@ public class Member {
 	
 	private Date enrollDate;
 
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		//로그인한 사용자의 권한을 설정하는 메소드
+		List<GrantedAuthority> auth=new ArrayList();
+		auth.add(new SimpleGrantedAuthority("ROLE_USER"));
+		if(userId.equals("admin")) {
+			auth.add(new SimpleGrantedAuthority("ROLE_ADMIN"));
+		}
+		return auth;
+	}
+
+	@Override
+	public String getUsername() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	
 }
