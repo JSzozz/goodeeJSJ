34663bc01aafdc259b9dc995cda3c11e970089f0
jsjyyy02190@gmail.com <tkdwns219@naver.com>
2023-07-25 10:32:40 +0900

    [security]6. 매핑(*SecurityController.java)
    1.
    @Controller
    @Slf4j
    public class SecurityController {
    
    	@GetMapping("/loginpage")
    	public String login() {
    		return "login";
    	}
    
    	@RequestMapping("/successLogin")
    	public String successLogin() {
    		log.debug("로그인 성공");
    		return "redirect:/";
    	}
    
    	@RequestMapping("/errorLogin")
    	public String errorLogin() throws IllegalAccessException {
    		throw new IllegalAccessException("잘못된 접근입니다");
    	}
    }

---
+++ src/main/java/com/bs/helloboot/controller/SecurityController.java
@@ -0,0 +1,28 @@
+package com.bs.helloboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Controller
+@Slf4j
+public class SecurityController {
+
+	@GetMapping("/loginpage")
+	public String login() {
+		return "login";
+	}
+	
+	@RequestMapping("/successLogin")
+	public String successLogin() {
+		log.debug("로그인 성공");
+		return "redirect:/";
+	}
+
+	@RequestMapping("/errorLogin")
+	public String errorLogin() throws IllegalAccessException {
+		throw new IllegalAccessException("잘못된 접근입니다");
+	}
+}
