d6fa5f5b4ec773b7e23f0805895e9ab832648869
jsjyyy02190@gmail.com <tkdwns219@naver.com>
2023-07-26 11:46:08 +0900

    [JPA]7.객체&테이블만들기2[@SequenceGenerator][@JsonIdentityInfo] (*JpaMember.j)
    1.@SequenceGenerator
     -> DB에서 사용할 sequence를 생성하는 어노테이션, ID값을 자동부여할 때 사용
    name 시퀀스 이름 / sequenceName DB에 등록될(된) 시퀀스 이름 / initValue allocationSize
    
    2.@TableGenerator
     -> ID값을 중복없이 발급하는 테이블 생성해서 ID부여하는 용도
    
    3.@JsonIdentityInfo
     -> Entity 객체를 가져올 때 양방향으로 일대다, 다대일 관계에 있으면 무한루핑이 발생하는데 이를 차단하는 어노테이션

--- src/main/java/com/bs/spring/jpa/entity/JpaMember.java
+++ src/main/java/com/bs/spring/jpa/entity/JpaMember.java
@@ -2,6 +2,7 @@ package com.bs.spring.jpa.entity;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -23,14 +24,22 @@ import lombok.NoArgsConstructor;
 //schema,catalog, 테이블레벨 제약조건 설정, DB에 생성될 테이블명 설정(생략하면 클래스 명으로 테이블 생성)
 //@Table()
 
+//@SequenceGenerator -> DB에서 사용할 sequence를 생성하는 어노테이션, ID값을 자동부여할 때 사용
+//name 시퀀스 이름 / sequenceName DB에 등록될(된) 시퀀스 이름 / initValue allocationSize
+
+//(+)@TableGenerator -> ID값을 중복없이 발급하는 테이블 생성해서 ID부여하는 용도
+
+//@JsonIdentityInfo -> Entity 객체를 가져올 때 양방향으로 일대다, 다대일 관계에 있으면 무한루핑이 발생하는데 이를 차단하는 어노테이션 
+
+//@Table(name="memberJpa")
 public class JpaMember {
 
 	@Id //entity를 식별하는 식별자, DB에서는 Primary key제약조건 설정
-	private Long memberNo;
-	private String memberId;
-	private String memberPwd;
+	private Long memberNo;//not null o
+	private String memberId;//not null x
+	private String memberPwd;//not null x
 	private Integer age;//not null x
 	//private int age;//not null o
-	private double height;
+	private double height;//not null o
 	
 }
