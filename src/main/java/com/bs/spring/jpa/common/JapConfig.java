cffca602d11f311eff074320180494174b657eb5
jsjyyy02190@gmail.com <tkdwns219@naver.com>
2023-07-26 10:42:31 +0900

    [JPA]5.세팅[EntityManager][EntityManagerFactory](*JapConfig.java)
    1.
    @Configuration
    public class JapConfig {
    
    	//jpa로 DB에 접속하기 위해서
    	//EntityManager객체가 필요함.
    	//EntityManager객체는 EntityManagerFactory클래스의
    	//createEntityManager()메소드를 이용해서 가져올 수 있다.
    	//EntityManagerFactory클래스를 가져오기 위해서는
    	//Persistence클래스의 static메소드인 createEntityManagerFactory()메소드를 이용한다.
    
    	@Bean
    	public EntityManagerFactory entityManagerFactory() {
    		EntityManagerFactory
    factory=Persistence.createEntityManagerFactory("bstest");
    		return factory;
    	}
    
    	@Bean
    	public EntityManager entityManager() {
    		return entityManagerFactory().createEntityManager();
    	}
    }

--- src/main/java/com/bs/spring/jpa/common/JapConfig.java
+++ src/main/java/com/bs/spring/jpa/common/JapConfig.java
@@ -1,5 +1,30 @@
 package com.bs.spring.jpa.common;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class JapConfig {
 
+	//jpa로 DB에 접속하기 위해서 
+	//EntityManager객체가 필요함.
+	//EntityManager객체는 EntityManagerFactory클래스의 
+	//createEntityManager()메소드를 이용해서 가져올 수 있다.
+	//EntityManagerFactory클래스를 가져오기 위해서는 
+	//Persistence클래스의 static메소드인 createEntityManagerFactory()메소드를 이용한다.
+	
+	@Bean
+	public EntityManagerFactory entityManagerFactory() {
+		EntityManagerFactory factory=Persistence.createEntityManagerFactory("bstest");
+		return factory;
+	}
+	
+	@Bean
+	public EntityManager entityManager() {
+		return entityManagerFactory().createEntityManager();
+	}
 }
